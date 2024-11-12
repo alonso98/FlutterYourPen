56a9309d2423c6e43f46c1c7c905fd18406e642f
Pol Yakov <polyakov.olexandr1998@gmail.com>
2024-11-12 22:43:19 +0100

    signin page finishing design

--- lib/views/onboarding/sign_in_page.dart
+++ lib/views/onboarding/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yourpen_1/controls/gradient_button.dart';
 import 'package:yourpen_1/controls/shadowed_textfield.dart';
 
 class SignInPage extends StatelessWidget {
@@ -6,6 +7,9 @@ class SignInPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final ColorScheme colorScheme = theme.colorScheme;
+
     return Scaffold(body: LayoutBuilder(
         builder: (BuildContext buildContext, BoxConstraints constraints) {
       return SingleChildScrollView(
@@ -48,24 +52,97 @@ class SignInPage extends StatelessWidget {
                               color: Color(0xff606060),
                               fontSize: 32,
                               fontFamily: "GentiumBasic"))
-                    ])
-                    ),
+                    ])),
                     const SizedBox(height: 50),
                     const Text("Please log in or create"),
                     const Text("new account"),
-                    const SizedBox(height: 40,),
+                    const SizedBox(
+                      height: 40,
+                    ),
                     const ShadowedTextfield(hintText: "Email"),
-                    const SizedBox(height: 12,),
-                    const ShadowedTextfield(hintText: "Password", isPassword: true,),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    const ShadowedTextfield(
+                      hintText: "Password",
+                      isPassword: true,
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                          style: TextButton.styleFrom(
+                            overlayColor: Colors.transparent,
+                            foregroundBuilder: (context, states, child) =>
+                                DecoratedBox(
+                              decoration: const BoxDecoration(
+                                border: Border(
+                                    bottom:
+                                        BorderSide(color: Color(0xff8545D8))),
+                              ),
+                              child: child,
+                            ),
+                          ),
+                          onPressed: () {},
+                          child: const Text("I forgot password",
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w600,
+                                  color: Color(0xff8545D8)))),
+                    ),
+                    const SizedBox(
+                      height: 56,
+                    ),
+                    GradientButton(
+                      onPressed: () {},
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 18, horizontal: 60),
+                      borderRadius: 30,
+                      gradient: const LinearGradient(
+                          colors: [Color(0xff9D68E4), Color(0xff8545D8)]),
+                      child: const Text(
+                        "SIGN IN",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
                   ],
                 ),
-                const Column(
+                Column(
                   children: <Widget>[
-                    SizedBox(
+                    const SizedBox(
                       height: 100,
                     ),
-                    Text("Bottom text"),
-                    SizedBox(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          "Don’t have an account yet?",
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        TextButton(
+                            style: TextButton.styleFrom(
+                              overlayColor: Colors.transparent,
+                              foregroundBuilder: (context, states, child) =>
+                                  DecoratedBox(
+                                decoration: const BoxDecoration(
+                                  border: Border(
+                                      bottom:
+                                          BorderSide(color: Color(0xff8545D8))),
+                                ),
+                                child: child,
+                              ),
+                            ),
+                            onPressed: () {},
+                            child: const Text("Sign up",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.w600,
+                                    color: Color(0xff8545D8))))
+                      ],
+                    ),
+                    const SizedBox(
                       height: 10,
                     ),
                   ],
